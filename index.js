7db3b35de9312a2dca2c994fdc9293312721fd49
Mona Lisa Octocat <raniamet.90@gmail.com>
2023-04-25 15:09:52 +0200

    Final Status

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-//Import the node-emoji library that we installed
 import emoji from 'node-emoji';
 
-//Get the emoji name that the user specified on the command line
-const emojiName = process.agrv[2];
+const emojiName = process.argv[2];
 
 if (!emojiName) {
   const randomEmoji = emoji.random();
-  //if the user did not specify an emoji, try display a random emoji
   console.log(randomEmoji.emoji);
 } else if (emojiName) {
-  //if the user did not specify an emoji, try to find that emoji
   const foundEmoji = emoji.find(emojiName);
-  //if
-}
-if (foundEmoji) {
-  //const randomEmoji
-  console.log(foundEmoji.emoji);
-} else {
-  console.log('Invalid emoji name');
+  if (foundEmoji) {
+    console.log(foundEmoji.emoji);
+  } else {
+    console.log('Invalid emoji name');
+  }
 }
